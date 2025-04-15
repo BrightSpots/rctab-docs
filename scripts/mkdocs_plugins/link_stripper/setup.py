d03fcfb82b5097b9c9e98a1d33d1baff8be1b7f4
Armin Samii <armin.samii@gmail.com>
2025-04-14 21:01:36 -0400

    fix link_stripper plugin

--- scripts/mkdocs_plugins/link_stripper/setup.py
+++ scripts/mkdocs_plugins/link_stripper/setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='link-stripper',
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'mkdocs==1.6.0',
         'beautifulsoup4==4.12.3'
-    ]
+    ],
+    entry_points={
+        "mkdocs.plugins": [
+            "link-stripper = link_stripper.link_stripper:LinkStripperPlugin",
+        ],
+    },
 )
 
